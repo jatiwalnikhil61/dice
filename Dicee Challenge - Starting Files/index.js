8c3eee703fa616dc26e595770c6aa614ff9da5df
jatiwalnikhil61 <73793976+jatiwalnikhil61@users.noreply.github.com>
2020-11-29 00:47:09 +0530

    Update index.js

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -7,12 +7,12 @@ document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber
 
 
 
-if(randomNumber1 === randomNumber2){
-  document.querySelector("h1").innerHTML = "Draw!";
+if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 }
 else if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 }
 else {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+  document.querySelector("h1").innerHTML = "Draw!";
 }
